1ea51922b581d06f524a6033ac592f0420f16258
vilasp29 <prabhu_vilas@yahoo.com>
2024-11-11 17:15:23 +0000

    Create config.js
    r

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
+    REPO_OWNER: process.env.vilasp29 || '',
+    REPO_NAME: process.env.SPDP25 || '',
+    FILE_PATH: process.env.FILE_PATH || 'rsvps.json'
+};
+
+export default config;
